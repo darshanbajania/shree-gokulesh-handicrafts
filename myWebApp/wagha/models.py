6f6d692d4f49f1095e4bd4ac29411c4c03aec374
darshanbajania <darshanbajania1999@gmail.com>
2023-08-12 19:26:45 +0530

    feat: size as foriengnkey

--- myWebApp/wagha/models.py
+++ myWebApp/wagha/models.py
@@ -2,6 +2,22 @@ import uuid
 from django.db import models
 from cloudinary.models import CloudinaryField
 # Create your models here.
+
+
+
+
+class Size(models.Model):
+    id = models.UUIDField(
+         primary_key = True,
+         default = uuid.uuid4,
+         editable = False)
+    label = models.CharField(max_length=200, default="size")
+
+    def __str__(self):
+        return f'{self.label}'
+    
+
+
 class Product(models.Model):
     id = models.UUIDField(
          primary_key = True,
@@ -11,6 +27,16 @@ class Product(models.Model):
     image = CloudinaryField('product_image', default="")
     description = models.CharField(max_length=1000, default="description")
     price = models.IntegerField(default=0)
+
+    def get_default_size():
+        """ get a UUID of a random size """
+        random_size = Size.objects.order_by('?').first()  # Get a random size
+        if random_size:
+            return random_size
+        else:
+            default_size, _ = Size.objects.get_or_create(label="created")
+            return default_size.id
+    size = models.ForeignKey(Size, on_delete=models.CASCADE, default=get_default_size  )
 
 
 class Rating(models.Model):
@@ -30,15 +56,7 @@ class Tag(models.Model):
 
     def __str__(self):
         return f'{self.label}'
-class Size(models.Model):
-    id = models.UUIDField(
-         primary_key = True,
-         default = uuid.uuid4,
-         editable = False)
-    label = models.CharField(max_length=200, default="size")
 
-    def __str__(self):
-        return f'{self.label}'
 class Color(models.Model):
     id = models.UUIDField(
          primary_key = True,
