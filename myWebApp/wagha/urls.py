2cf5efa92434c23b7a489bbe51c7e8e3469a7c46
darshanbajania <darshanbajania1999@gmail.com>
2023-08-20 15:50:33 +0530

    feat: product details page integration

--- myWebApp/wagha/urls.py
+++ myWebApp/wagha/urls.py
@@ -3,6 +3,9 @@ from django.urls import path
 
 from . import views
 
+app_name = 'wagha'
+
 urlpatterns = [
-    path('', views.home, name='index'),
+    path('', views.Home, name='home'),
+    path('product_details', views.Product_details, name='product_details'),
 ]
--- myWebApp/wagha/views.py
+++ myWebApp/wagha/views.py
@@ -2,16 +2,44 @@ from django.shortcuts import render
 from .models import Product, Advertisement
 # Create your views here.
 from django.http import HttpResponse
-
-
-def home(request):
+from django.urls import reverse
+from django.shortcuts import render, redirect
+from urllib.parse import urlencode
+def Home(request):
     product_data = Product.objects.all()
     homeAdvertisementImage = Advertisement.objects.filter(label='HomePage').first()
-    print("🚀 ~ file: views.py:10 ~ homeAdvertisementImage:", homeAdvertisementImage.image.url)
 
+
+    if request.method == "POST":
+            # cart_contents.clear()
+            # print(cart_contents)
+            # print(request.session['cart_contents'])
+        product_id = request.POST.get('product')
+        print("🚀 ~ file: views.py:17 ~ product_number:", product_id)
+        if product_id != None:
+            # getting the url for displaying full proposal
+            print(product_id)
+            base_url = reverse('wagha:product_details')
+            # creating a string of dictionary
+            query_string = urlencode({'product': product_id})
+            # passing the base_url and query from this page to url
+            url = '{}?{}'.format(base_url, query_string)
+
+            # print(proposal_number)
+            return redirect(url)
     context ={
         'product_data': product_data,
         'banner_url':homeAdvertisementImage.image.url
     }
-
     return render(request, 'wagha/base.html', context=context)
+
+def Product_details(request):
+    current_product_id = request.GET.get('product')
+        # print(current_product_id)
+    selected_product = Product.objects.filter(id=current_product_id).first()
+    print("🚀 ~ file: views.py:39 ~ selected_product:", selected_product)
+    context ={
+        'selected_product': selected_product,
+    }
+
+    return render(request, 'wagha/product_details.html', context=context)
