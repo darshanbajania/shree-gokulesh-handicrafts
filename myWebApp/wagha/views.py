2f6daf65f1af37cd22a994a67ba02fa846067dc9
darshanbajania <darshanbajania1999@gmail.com>
2023-08-20 13:03:36 +0530

    product integration with backend

--- myWebApp/wagha/views.py
+++ myWebApp/wagha/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
-
+from .models import Product
 # Create your views here.
 from django.http import HttpResponse
 
 
-def index(request):
-    return render(request, 'wagha/index.html')
+def home(request):
+    product_data = Product.objects.all()
+    print("🚀 ~ file: views.py:9 ~ productData:", product_data)
+    context ={
+        'product_data': product_data
+    }
+    return render(request, 'wagha/base.html', context=context)
