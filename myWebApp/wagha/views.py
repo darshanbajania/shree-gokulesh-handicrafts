ca755c7b13e25f6f2610ead74708baa9ab2c0ad7
darshanbajania <darshanbajania1999@gmail.com>
2023-09-16 13:44:57 +0530

    feat: cart local storage setup

--- myWebApp/wagha/views.py
+++ myWebApp/wagha/views.py
@@ -5,10 +5,27 @@ from django.http import HttpResponse
 from django.urls import reverse
 from django.shortcuts import render, redirect
 from urllib.parse import urlencode
+import json
+
+class cart_product:
+    def __init__(self, product_id, quantity):
+        self.product_id = product_id
+        self.quantity = quantity
+
+ 
+class CartProductEncoder(json.JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, cart_product):
+            return {'product_id': obj.product_id, 'quantity': obj.quantity}
+        return super().default(obj)
+
+
+
 def Home(request):
     product_data = Product.objects.all()
     homeAdvertisementImage = Advertisement.objects.filter(label='HomePage').first()
-
+    if(request.session.has_key('cart_data') != True):
+        request.session['cart_data'] = {} 
 
     if request.method == "POST":
             # cart_contents.clear()
@@ -16,7 +33,9 @@ def Home(request):
             # print(request.session['cart_contents'])
         product_id = request.POST.get('product')
         view_all_type = request.POST.get('view-all-button')
-        print("🚀 ~ file: views.py:17 ~ product_number:", product_id)
+        card_action_type = request.POST.get('card_action_type')
+
+                    
         if view_all_type != None:
             print(product_id)
             base_url = reverse('wagha:all_products')
@@ -27,7 +46,20 @@ def Home(request):
 
             # print(proposal_number)
             return redirect(url)
-        if product_id != None:
+
+        if card_action_type == 'add-to-cart' and product_id != None :
+            if(request.session.has_key('cart_data')):
+
+                cartData = request.session['cart_data']
+                print("🚀 ~ file: views.py:43 ~ cartData:", cartData)
+                cartContents = cartData
+                product = cart_product(product_id, 1)
+                serialized_product = json.dumps(product, cls=CartProductEncoder)
+                cartContents[product_id]=serialized_product
+                request.session['cart_data'] = cartContents
+                print("🚀 ~ file: views.py:37 ~ cartProduct:", cartContents)
+
+        if card_action_type == 'view-product' and product_id != None:
             # getting the url for displaying full proposal
             print(product_id)
             base_url = reverse('wagha:product_details')
@@ -61,7 +93,6 @@ def All_products(request):
             # print(cart_contents)
             # print(request.session['cart_contents'])
         product_id = request.POST.get('product')
-
         if product_id != None:
             # getting the url for displaying full proposal
             print(product_id)
